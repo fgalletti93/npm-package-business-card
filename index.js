1ea08a2f4f9516eed9e76ee1b45d33c4ba989a14
fgalletti93 <fgalletti93@yahoo.com>
2022-02-19 07:53:49 -0300

    complete npm package

---
+++ index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+console.log(`
+Francesca Galletti
+Linkedin - https://www.linkedin.com/in/fgalletti93/
+Github - https://github.com/fgalletti93
+Latest Projects 
+    - https://pokedex-theta-murex.vercel.app/
+    - https://aluracord-matrix-blond-phi.vercel.app/
+    - https://fgalletti93.github.io/drumkit/
+Email - fgalletti93@yahoo.com
+`)
